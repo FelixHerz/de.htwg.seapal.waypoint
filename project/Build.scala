72d15951fdc17199704c3b938c1ab8d0ca382cce
Cris <cristofo_vilares@gmx.ch>
2013-04-10 10:30:21 +0200

    changed Build.scala for jenkins

--- project/Build.scala
+++ project/Build.scala
@@ -4,19 +4,40 @@ import play.Project._
 
 object ApplicationBuild extends Build {
 
-  val appName         = "waypoint"
-  val appVersion      = "1.0-SNAPSHOT"
+	val appName         = "waypoint"
+	val appVersion      = "1.0-SNAPSHOT"
 
-  val appDependencies = Seq(
-    // Add your project dependencies here,
-    javaCore,
-    javaJdbc,
-    javaEbean,
-    "com.google.inject" % "guice" % "3.0"
-  )
+
+
+	val appDependencies = Seq(
+	    javaCore,
+	    javaJdbc,
+	    javaEbean,
+		"com.google.inject" % "guice" % "3.0",
+		"com.google.inject.extensions" % "guice-multibindings" % "3.0",
+		"util" % "util_2.10" % "1.0-SNAPSHOT"
+	)
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
-  )
+  	
+	// group id...
+	organization := "de.htwg.seapal", 
 
+	// disable using the Scala version in output paths and artifacts
+	crossPaths := false, 
+
+	resolvers += "HTWG Resolver" at "http://lenny2.in.htwg-konstanz.de:8081/artifactory/libs-snapshot-local",	
+	publishTo := Some(	"HTWG Publisher" at "http://lenny2.in.htwgkonstanz.de:8081/artifactory/libs-snapshot-local;build.timestamp=" + 
+	new java.util.Date().getTime()),
+		pomExtra :=
+			<build>
+				<sourceDirectory>app</sourceDirectory>
+				<testSourceDirectory>test</testSourceDirectory>
+				<resources>
+					<resource>
+						<directory>app</directory>
+					</resource>
+				</resources>
+			</build>	
+	)
 }
